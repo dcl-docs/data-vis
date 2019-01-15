1f9d8180af96b9a859eb4970c4af98e68f9250dc
Sara Altman <skaltman@stanford.edu>
2019-01-14 19:44:05 -0800

    add stamp function

--- _common.R
+++ _common.R
@@ -14,3 +14,30 @@ knitr::opts_chunk$set(
   fig.asp = 0.618,  # 1 / phi
   fig.show = "hold"
 )
+
+# Stamps plots with a tag and line 
+# Idea from Claus Wilke's "Data Visualization" https://serialmentor.com/dataviz/
+stamp <- function(tag = "bad", tag_color = "#B33A3A", tag_size = 16, tag_padding = 1,
+                  line_color = "#B33A3A", line_size = 3)
+{
+  list(
+    annotate(
+      geom = "segment",
+      x = Inf,
+      xend = Inf,
+      y = -Inf,
+      yend = Inf,
+      color = line_color,
+      size = line_size
+    ),
+    theme(
+      plot.tag = element_text(color = tag_color, size = tag_size),
+      plot.tag.position = "topright"
+    ),
+    labs(
+      tag =
+        str_pad(tag, width = str_length(tag) + tag_padding, side = "left")
+    )
+  )
+}
+
